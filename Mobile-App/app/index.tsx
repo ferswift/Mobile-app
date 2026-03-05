c8abaf82e3c5e4c739fc0ad82d2bb6cef6cdcf0d
ferswift <nandofhen@gmail.com>
2026-03-05 14:38:44 -0300

    added styles and view

--- Mobile-App/app/index.tsx
+++ Mobile-App/app/index.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Logo from "../src/assets/img/expo2.png";
 
 export default function index() {
   return (
-    <View>
-      <Text>Olá Mundo</Text>
+    <View style={styles.container}>
+      <Image source={Logo} />
+      <Text style={styles.title}>The number One</Text>
+      <Text style={styles.p}>Reading List App</Text>
+
+      <View>
+        <Text style={styles.card}>Card Application</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    backgroundColor: "#21033F",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginTop: 50,
+  },
+  card: {
+    backgroundColor: "#61DAFB",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+    boxShadow: "0 4px 10px rgba(0,0,0,0.8)",
+  },
+  p: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+  },
+});
