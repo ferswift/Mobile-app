4a307e1a39523bc75db1b2845ee3d66edad3c951
ferswift <nandofhen@gmail.com>
2026-03-31 13:50:22 -0300

    Login Persistence

--- Mobile-App/app/index.tsx
+++ Mobile-App/app/index.tsx
@@ -2,8 +2,27 @@ import { Link } from "expo-router";
 import { Image, StyleSheet, View } from "react-native";
 import ThemedView from "@/src/components/ThemedView";
 import ThemedText from "@/src/components/ThemedText";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "@/src/services/firebaseconfig";
 
 export default function Index() {
+  // persistencia de login
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <ThemedView style={styles.container}>
       <Image source={require("../src/assets/img/expo2.png")} />
